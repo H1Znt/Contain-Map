0a0b4512724d9bd04c07af850ecd7d33cfc8fb41
Andrey Maximov <arman.gen40@mail.ru>
2024-10-22 18:45:39 +0300

    Add tests

--- src/js/__tests__/contains-map.test.js
+++ src/js/__tests__/contains-map.test.js
@@ -1,1 +1,19 @@
-import Team from "../contains-map";
+import ErrorRepository from "../contains-map";
+
+const errorRepository = new ErrorRepository();
+
+test("Добавление элемента в map", () => {
+  errorRepository.addTranslation(404, 'Not Found');
+  expect(errorRepository.errors.size).toBe(1)
+})
+
+test("Поиск элемента в map", () => {
+  const result = errorRepository.translate(404);
+  expect(result).toBe('Not Found')
+})
+
+test("Проверка вывода ошибки", () => {
+  expect(() => {
+    errorRepository.translate(101)
+  }).toThrow()
+})
